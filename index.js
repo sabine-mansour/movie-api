c97273a339921205005c094e1bd7f61e62f83100
sabine-mansour <sabine_mansour@icloud.com>
2021-04-07 11:03:28 +0200

    Created REST API requests

--- index.js
+++ index.js
@@ -4,40 +4,21 @@ morgan = require('morgan');
 const app = express();
 
 
-let topMovies = [
+let movies = [
   {
-    title: 'The Godfather'
-  },
-  {
-    title: 'Mad Max: Fury Road'
-  },
-  {
-    title: 'Moonlight'
-  },
-  {
-    title: 'Brokeback Mountain'
-  },
-  {
-    title: 'Jaws'
-  },
-  {
-    title: 'Iron Man'
-  },
-  {
-    title: 'Mean Girls'
-  },
-  {
-    title: '12 Angry Men'
-  },
-  {
-    title: 'Get Out'
-  },
-  {
-    title: 'The Good, the Bad and the Ugly'
+    title: 'Black Panther',
+    description: 'Black Panther is a 2018 American superhero film based on the Marvel Comics character of the same name. ',
+    director: 'Ryan Koogler',
+    genre:'Adventure',
+    image: 'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQPpcKQ9eWZGxJe6eXyCW91eayLVm4KqruvJz3GP0F2T2w46yKZ',
+    featured: true
   }
 ]
 
 //Middleware
+
+app.use(bodyParser.json());
+
 app.use(morgan('common'));
 
 app.use(express.static('public'));
@@ -54,8 +35,43 @@ app.get('/', (req, res) => {
 });
 
 app.get('/movies', (req, res) => {
-  res.json(topMovies);
+  res.json(movies);
 });
+
+app.get('/movies/:title', (req, res) => {
+  res.json(movies.find((movie) => {
+  return movie.title === req.params.title
+  }));
+});
+
+app.get('/movies/genres/:genre', (req, res) => {
+  res.send('Successful GET request returning a description of the genre')
+});
+
+app.get('/movies/directors/:name', (req, res) => {
+  res.send('Successful GET request returning a description of the Director')
+});
+
+app.post('/users', (req, res) => {
+  res.send('Registration succesful!')
+});
+
+app.put('/users/:username', (req, res) => {
+ res.send('The user: ' + req.params.username + ' was successfully updated')
+});
+
+app.post('/users/:username/favourites', (req, res) => {
+  res.send('Movie: ' + req.params.title + ' was added to favourites.');
+  });
+
+app.delete('/users/:username/favourites/:title', (req, res) => {
+ res.send('Movie: ' + req.params.title + ' was removed from favourites.');
+});
+
+app.delete('/users/:username', (req, res) => {
+  res.send('User ' + req.params.id + ' was deleted.');
+  });
+
 
 //Listen for requests
 app.listen(8080, () => {
