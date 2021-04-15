55449a9daed2fb7a4c403268ad3012aac2ca8634
sabine-mansour <sabine_mansour@icloud.com>
2021-04-15 18:17:10 +0200

    Updated mongoose.connect

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const Movies = Models.Movie;
 const Users = Models.User;
 
 //mongoose.connect('mongodb://localhost:27017/movieApiDB', {useNewUrlParser: true, useUnifiedTopology: true});
-mongoose.connect('process.env.CONNECTION_URI', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(process.env.CONNECTION_URI, {useNewUrlParser: true, useUnifiedTopology: true});
 
 const bodyParser = require('body-parser');
 
